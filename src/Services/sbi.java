a0f8bbdaafddeaacfdebdd5d104e7b96f76e93ee
swami raj <swamiraj0303@gmail.com>
2023-12-15 17:25:57 +0530

    Project

--- src/Services/sbi.java
+++ src/Services/sbi.java
@@ -1,6 +1,8 @@
 package Services;
 
 import POJO.AccountDetails;
+import Exception.InvalidAmount;
+import Exception.insufficientBalanceException;
 
 public class sbi implements BankAccounts{
     private long accountno;
@@ -9,26 +11,47 @@ public class sbi implements BankAccounts{
 
     @Override
     public void init(AccountDetails accountDetails) {
+        this.accountno=accountDetails.getAccountno();
+        this.username=accountDetails.getUsername();
+        this.balance=accountDetails.getBalance();
+        System.out.println("SBI rocket");
+    }
+
+    @Override
+    public void deposit(double amount)throws InvalidAmount {
+        if(amount<=0){
+            throw new InvalidAmount("Enter amount grater than 0");
+        }
+        balance=balance+amount;
+        System.out.println("Amount debited from SBI");
 
     }
 
     @Override
-    public void deposit(double amount) {
-
-    }
-
-    @Override
-    public void withdraw(double amount) {
+    public void withdraw(double amount) throws InvalidAmount,insufficientBalanceException {
+        if(amount<=0){
+            throw new InvalidAmount("Enter amount grater than 0");
+        }
+        if(amount>balance){
+            throw new insufficientBalanceException("Insufficient balance");
+        }
+        balance=balance-amount;
+        System.out.println("Amount debited from SBI");
 
     }
 
     @Override
     public void balancenquery() {
-
+        System.out.println(balance);
+        System.out.println("Operation Done on SBI");
     }
 
     @Override
-    public void moneytransfer(BankAccounts account,double amount) {
+    public void moneytransfer(BankAccounts account,double amount)throws InvalidAmount,insufficientBalanceException {
+        withdraw(amount);
+
+        deposit(amount);
+        System.out.println("Operation done on SBI Account");
 
     }
 }
